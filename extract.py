3dc3f1860e54f747f0ceef53bcc1f16d4ac2ec99
Isaac Kobby Anni <annqwabena@gmail.com>
2022-04-09 13:40:45 +0000

    edited

--- extract.py
+++ extract.py
@@ -1,44 +1,61 @@
 import re
+import os
 
-def read_xml(xml_file, element):
-    """
-    Return only needed information
-    """
-    # get the beginning tag of the needed content
-    start_tag = f'<{element}>' 
+class readXML:
+    def __init__(self, element, input_folder, output_folder) -> None:
+        self.element = element
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.xml_files = [os.path.join(input_folder, xml) for xml in os.listdir(input_folder) if xml.endswith('.xml')]
+    
+    def _get_files(self):
+        for file in self.xml_files:
+            self.read_xml(file, self.element)
 
-    # the end tag of the content
-    end_tag = f'</{element}>'
+    def read_xml(self, xml_file, element):
+        """
+        Return only needed information
+        """
+        # get the beginning tag of the needed content
+        start_tag = f'<{element}>' 
 
-    # to keep track of finding the required head
-    start_tag_identified = False
+        # the end tag of the content
+        end_tag = f'</{element}>'
 
-    # record all the words 
-    captured_records = []
+        # to keep track of finding the required head
+        start_tag_identified = False
 
-    # record word by each line
-    captured_word = ''
+        # record all the words 
+        captured_records = []
 
-    # open the file and iterate through
-    with open(xml_file) as f:
-        for line in f:
-            if start_tag in line:
-                start_tag_identified = True
-            if start_tag_identified:
-                split = re.split('>|<', line)
-                word = split[2]
-                captured_word += word
-            if end_tag in line:
-                captured_records.append(captured_word)
-                start_tag_identified = False
-                captured_word = ''
+        # record word by each line
+        captured_word = ''
 
-    # join all words to form one text
-    text = " ".join(captured_records)
+        # open the file and iterate through
+        with open(xml_file) as f:
+            for line in f:
+                if start_tag in line:
+                    start_tag_identified = True
+                if start_tag_identified:
+                    split = re.split('>|<', line)
+                    word = split[2]
+                    captured_word += word
+                if end_tag in line:
+                    captured_records.append(captured_word)
+                    start_tag_identified = False
+                    captured_word = ''
 
-    # save file to disk with the same file name in txt format.
-    prefix = xml_file.split('.')[0]
-    output_name = f'{prefix}_text_result.txt'
-    with open(output_name,  'w') as text_df:
-            text_df.write(text)
+        # join all words to form one text
+        text = " ".join(captured_records)
 
+        # save file to disk with the same file name in txt format.
+        prefix = re.split('/|\.', xml_file)[-2]
+        output_name = f'{self.output_folder}/{prefix}_text_result.txt'
+        with open(output_name,  'w') as text_df:
+                text_df.write(text)
+
+if __name__ == "__main__":
+    in_folder = '/Users/sugar/Documents/icrisat/test'
+    out_folder = '/Users/sugar/Documents/icrisat/test/results'
+    reader = readXML('word', in_folder, out_folder)
+    reader._get_files()
